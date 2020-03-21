373d002fff92e12fb2484d7469957727114ae049
Kevin <kevinihm@gmail.com>
2020-03-20 22:58:54 -0700

    DataTable now conditionally renders dataView or data depending on if dataView is empty or not

--- client/components/dataTable.jsx
+++ client/components/dataTable.jsx
@@ -9,23 +9,24 @@ export default class DataTable extends Component {
   }
 
   render() {
-    const { data } = this.props;
+    const { data, dataView } = this.props;
     return (
       <main className="data-container container">
         <section className="row">
+          <h5>INFECTIONS</h5>
           <table className="table">
             <thead>
               <tr>
                 <th scope="col">Country Code</th>
                 <th scope="col">Country</th>
                 <th scope="col">Province</th>
-                <th scope="col">Confirmed Infected</th>
+                <th scope="col">Confirmed</th>
                 <th scope="col">Recovered</th>
                 <th scope="col">Death(s)</th>
               </tr>
             </thead>
             <tbody>
-              {data.map((val, i) => {
+              {dataView.length === 0 ? data.map((val, i) => {
                 return (
                   <tr key={i}>
                     <th scope="row">{val.country_code}</th>
