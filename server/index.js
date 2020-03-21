57739cbe090c7f250d41de4ca4aff703f84a97e2
Kevin <kevinihm@gmail.com>
2020-03-20 23:37:12 -0700

    minor changes to server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT;
 app.use(express.static(staticPath));
 app.use(express.json());
 
-app.get('/', (req, res) => res.render('main'));
+app.get('/', (req, res) => {
+  res.render('main');
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
