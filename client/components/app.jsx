ca2acb9014f5ed84c53e250e24ec8d8d1b8bde44
Kevin <kevinihm@gmail.com>
2020-03-23 18:25:11 -0700

    passed USData as props to WorldMap

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -82,13 +82,13 @@ export default class App extends Component {
   }
 
   render() {
-    const { data, dataView, searchInput, showAll, content, setContent, countryCodeData } = this.state;
+    const { data, dataView, searchInput, showAll, content, setContent, countryCodeData, USData } = this.state;
     const mapWidth = 1080, height = mapWidth / 2;
     if (data.length === 0) return <div>LOADING...</div>
     return (
       <>
         <Header />
-        <WorldMap countryCodeData={countryCodeData} data={data} setTooltipContent={this.setTooltipContent} />
+        <WorldMap countryCodeData={countryCodeData} USData={USData} data={data} setTooltipContent={this.setTooltipContent} />
         <ReactTooltip>{content}</ReactTooltip>
         <main className="search-container container">
           <section className="row">
