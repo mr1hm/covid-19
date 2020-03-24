f885b46cf7f4b742571c6657e3b1162c303efd98
Kevin <kevinihm@gmail.com>
2020-03-24 00:19:12 -0700

    added conditional to regionData component

--- client/components/regionData.jsx
+++ client/components/regionData.jsx
@@ -7,8 +7,14 @@ export default class RegionData extends Component {
 
   render() {
     const { regionData } = this.props;
+    if (!regionData) {
+      return (
+        <div>Please select a state to view data</div>
+      );
+    }
     return (
       <div className="col-4 d-flex flex-column region-data">
+        <i className="fas fa-long-arrow-alt-right"></i>
         <h4>{regionData.stateName}</h4>
         <p><span className="infections">Infections</span>: {regionData.infected}</p>
         <p><span className="recovered">Recovered</span>: {regionData.recovered}</p>
