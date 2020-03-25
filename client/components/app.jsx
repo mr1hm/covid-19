16760cac4b2f46ee9174798937a3e68972d351b8
Kevin <kevinihm@gmail.com>
2020-03-24 17:19:03 -0700

    imported WorldMap into app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './layout/header';
 import DataTable from './dataTable';
 import USMap from './maps/USMap';
+import WorldMap from './maps/WorldMap';
 import ReactTooltip from 'react-tooltip';
 import abbrState from './stateHelper';
 import { countryListObjByCode } from './countries';
