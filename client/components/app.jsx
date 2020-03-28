0c785e2fc6d810efbd70b0cfbf5598dc0e7606c7
Kevin <kevinihm@gmail.com>
2020-03-27 23:44:58 -0700

    added news as state property to app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -14,6 +14,7 @@ export default class App extends Component {
     this.state = {
       data: [],
       dataView: [],
+      news: [],
       searchInput: '',
       showAll: false,
       countriesColorData: null,
