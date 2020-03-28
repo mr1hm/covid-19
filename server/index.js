cba3476f1320cfdf5e042a4632d6f9bfdef551d8
Kevin <kevinihm@gmail.com>
2020-03-27 19:02:13 -0700

    required queries.js file in index.js and added .get and .post routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const staticPath = path.join(__dirname, 'public');
 const port = process.env.PORT;
+const db = require('./api/queries');
 
 app.use(express.static(staticPath));
 app.use(express.json());
@@ -11,5 +12,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.render('main');
 });
+
+app.get('/api/lastUpdated', db.getLastUpdated);
+app.post('/api/lastUpdated', db.storeLastUpdated);
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
