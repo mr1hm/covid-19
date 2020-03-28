475aac8bf45a120e37ebe8fe2a2d42cb230a6d9c
Kevin <kevinihm@gmail.com>
2020-03-28 14:55:10 -0700

    added bootstrap card html in news.jsx

--- client/components/news.jsx
+++ client/components/news.jsx
@@ -22,17 +22,22 @@ export default class News extends Component {
   render() {
     const { news } = this.props;
     return (
-      <main className="news-container container">
-        <section className="row flex-column">
-          <div className="col d-flex flex-column">
-            {news.map((article, i) => {
-              return (
-                <div className="article">
-                  <h5><a href={article.url} target="_blank">{article.title}</a></h5>
+      <main className="news-container container-fluid">
+        <section className="row justify-content-center">
+          {news.map((article, i) => {
+            return (
+              <div key={i} className="col-3 d-flex card-article">
+                <div className="card">
+                  {article.urlToImage ? <img className="card-img-top article-card-img" src={article.urlToImage} alt="Card image cap" /> : <img className="card-img-top" src={article.urlToImage} alt="No Image Available" />}
+                  <div className="card-body">
+                    <h5 className="card-title">{article.title}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href={article.url} target="_blank" className="btn btn-primary">Go to Article</a>
+                  </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </section>
       </main>
     );
