026d283a11908b8df4b99011509755f4e0d0ce65
Kevin <kevinihm@gmail.com>
2020-03-28 18:45:31 -0700

    minor tweaks to html

--- client/components/news.jsx
+++ client/components/news.jsx
@@ -28,11 +28,11 @@ export default class News extends Component {
           {news.map((article, i) => {
             const articleTitle = article.title.split(' - ')[0];
             return (
-              <div key={i} className="col-3 d-flex card-article">
+              <div key={i} className="col-3 col-xl-3 d-flex card-article">
                 <div className="card">
-                  {article.urlToImage ? <img className="card-img-top article-card-img" src={article.urlToImage} alt="Card image cap" /> : <img className="card-img-top" src={article.urlToImage} alt="No Image Available" />}
+                  {article.urlToImage ? <img className="card-img-top article-card-img" src={article.urlToImage} alt="article-image" /> : <img className="card-img-top" src={article.urlToImage} alt="No Image Available" />}
                   <div className="card-body">
-                    <h5 className="card-title article-source"><a href={article.url} target="_blank" className="article-source-name">{article.source.name.toUpperCase()}<i class="fas fa-angle-double-right article-source-arrow"></i></a></h5>
+                    <h5 className="card-title article-source"><a href={article.url} target="_blank" className="article-source-name">{article.source.name.toUpperCase()}<i className="fas fa-angle-double-right article-source-arrow"></i></a></h5>
                     <p className="card-text article-title">{articleTitle}</p>
                   </div>
                 </div>
