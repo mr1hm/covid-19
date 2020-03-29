06fba65002376942297daf5630638eb68868c1ae
Kevin <kevinihm@gmail.com>
2020-03-28 17:04:54 -0700

    minor changes to html in news component

--- client/components/news.jsx
+++ client/components/news.jsx
@@ -24,18 +24,18 @@ export default class News extends Component {
     return (
       <main className="news-container container-fluid">
         <section className="row tabs">
-          {/* INSERT TABS COMPONENT HERE */}
+
         </section>
         <section className="row justify-content-center">
           {news.map((article, i) => {
+            const articleTitle = article.title.split(' - ')[0];
             return (
               <div key={i} className="col-3 d-flex card-article">
                 <div className="card">
                   {article.urlToImage ? <img className="card-img-top article-card-img" src={article.urlToImage} alt="Card image cap" /> : <img className="card-img-top" src={article.urlToImage} alt="No Image Available" />}
                   <div className="card-body">
-                    <h6 className="card-title article-title">{article.title.toUpperCase()}</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href={article.url} target="_blank" className="btn btn-primary">Go to Article</a>
+                    <h5 className="card-title article-source"><a href={article.url} target="_blank" className="article-source-name">{article.source.name.toUpperCase()}<i class="fas fa-angle-double-right article-source-arrow"></i></a></h5>
+                    <p className="card-text article-title">{articleTitle}</p>
                   </div>
                 </div>
               </div>
