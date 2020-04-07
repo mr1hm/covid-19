85996842dded00af4535a33f0fc80fa64d0808e5
Kevin Ihm <kevinihm@gmail.com>
2020-04-06 17:46:09 -0700

    commented out unused code

--- server/api/queries.js
+++ server/api/queries.js
@@ -1,22 +1,33 @@
 const Pool = require('pg').Pool;
 const conn = require('./db_conn');
-const ClientError = require('./client-error');
 
-const getLastUpdated = (req, res) => {
-  conn.query(`SELECT * FROM lastUpdated ORDER BY id ASC`, (err, results) => {
-    console.log(results.rows);
-    if (err) throw err;
-    res.status(200).json(results.rows);
-  })
+const getLastUpdated = (req, res, next) => {
+  conn.query(`select * from lastupdated order by id ASC`)
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
 }
 
-const storeLastUpdated = (req, res) => {
-  const { lastUpdated } = req.body;
-  conn.query(`INSERT INTO users (lastUpdated) VALUES ($1)`, [lastUpdated], (err, results) => {
-    if (err) throw err;
-    res.status(201).send(`LastUpdated stored with ID: ${results.insertId}`);
-  })
+// const getLastUpdated = (req, res) => {
+//   conn.query(`SELECT * FROM lastUpdated ORDER BY id ASC`, (err, results) => {
+//     console.log(results.rows);
+//     if (err) throw err;
+//     res.status(200).json(results.rows);
+//   })
+// }
+
+const storeLastUpdated = (req, res, next) => {
+  conn.query(`INSERT INTO lastupdated (datetime) VALUES ($1)`, [datetime])
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
 }
+
+// const storeLastUpdated = (req, res) => {
+//   const { lastUpdated } = req.body;
+//   conn.query(`INSERT INTO users (lastUpdated) VALUES ($1)`, [lastUpdated], (err, results) => {
+//     if (err) throw err;
+//     res.status(201).send(`LastUpdated stored with ID: ${results.insertId}`);
+//   })
+// }
 
 module.exports = {
   getLastUpdated,
