240e7c9642bc9b80b537b6b3845438cd4fdcc432
Kevin Ihm <kevinihm@gmail.com>
2020-05-26 14:14:54 -0700

    added 'returning *' to UPDATE query

--- server/api/queries.js
+++ server/api/queries.js
@@ -17,7 +17,7 @@ const getLastUpdated = (req, res, next) => {
 
 const storeLastUpdated = (req, res, next) => {
   const { unixTimestamp, infections, recovered, deaths, increased } = req.body;
-  conn.query(`UPDATE lastupdated SET datetime = ($1), infections = ($2), recovered = ($3), deaths = ($4), increased = ($5) WHERE id = 1`, [unixTimestamp, infections, recovered, deaths, increased], (err, results) => {
+  conn.query(`UPDATE lastupdated SET datetime = ($1), infections = ($2), recovered = ($3), deaths = ($4), increased = ($5) WHERE id = 1 returning *`, [unixTimestamp, infections, recovered, deaths, increased], (err, results) => {
     if (err) throw err;
     res.status(201).json({
       insertID: results.insertId
